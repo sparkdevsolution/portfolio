d5de121dcf1a0d53f1620865db15383fd40a1dcd
Prakash <prakashku5566singh@gmail.com>
2025-02-10 13:09:00 +0530

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-    return {
-        build: {
-            outDir: 'build',
-        },
-        plugins: [react()],
-    };
+// export default defineConfig(() => {
+//     return {
+//         build: {
+//             outDir: 'build',
+//         },
+//         plugins: [react()],
+//     };
+// });
+
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // You can change the port here
+  },
+  resolve: {
+    alias: {
+      "@": "/src", // Simplified import paths
+    },
+  },
 });
